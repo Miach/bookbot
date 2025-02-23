91e802856782534dbe8d8beee9b9cb4f13f0b958
Miach <michaelh8056@gmail.com>
2025-02-23 15:19:35 -0800

    add the char count function to stats.py and c all it from main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import count_characters
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
@@ -7,5 +9,7 @@ def get_book_text(file_path):
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
     count_words(file_contents)
+    count_characters(file_contents)
+
 
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,13 @@ def count_words(file_contents):
     words = file_contents.split()
     word_length = len(words)
     print(f"{word_length} words found in the document")
+
+def count_characters(file_contents):
+    file_contents = file_contents.lower()
+    unique_characters = {}
+    for char in file_contents:
+        if char in unique_characters:
+            unique_characters[char] += 1
+        else:
+            unique_characters[char] = 1
+    print(unique_characters)
