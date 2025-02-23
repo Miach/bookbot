30b4cf8efc309b4e8ebe21885e20988ca9c43f19
Miach <michaelh8056@gmail.com>
2025-02-23 14:12:51 -0800

    update main.py with read output to string with two functions.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+def get_book_text(file_path):
+    with open(file_path) as f:
         file_contents = f.read()
+    return file_contents
+
+
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    print(file_contents)
 
 main()
